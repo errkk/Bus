bc9648a07e47d586cd21d8bd52195c3b734dd4e6
Eric <errkkgeorge+github@gmail.com>
2011-09-13 22:16:59 +0100

    Holy fuck I've just done a closure!! there awesome!
    what actually has happened here is i made a thing to find where the
    client is, check the web service for bus stops and plot them on a map.
    Added event listener to marker objects to pull the arrival details for
    that stop. Hard coded at 88 for now. Thats the most important one

--- app/server.py
+++ app/server.py
@@ -9,13 +9,20 @@ from bottle import route,run,static_file,template,debug
 @route('/bus/:busstop/:number')
 def bus(busstop = 51502,number=88):
 #    busstop = 51502
-    url = 'http://countdown.tfl.gov.uk/stopBoard/%s' % busstop
+    url = 'http://countdown.tfl.gov.uk/stopBoard/%s?_dc=1315936072189' % busstop
 
-    result = simplejson.load(urllib.urlopen(url))
+    jsonfile = urllib.urlopen(url)
+
+    result = simplejson.load( jsonfile )
+
+    if result:
+        print 'JSON'
+    else:
+        print 'Stops JSON Fail'
+
     arrivals = result['arrivals']
 
     found = False
-    string = ''
 
     buses = []
 
@@ -27,9 +34,39 @@ def bus(busstop = 51502,number=88):
 
             buses.append( {'route':item['routeName'],'wait':item['estimatedWait']} )
 
-    return json.dumps( buses )
+    if found:
+        return json.dumps( buses )
+    else:
+        return 'Fail'
 
+@route('/stops/:lat/:lng')
+def stops( lat=51.4612,lng=-0.1402 ):
+    print '%s - %s' % ( lat,lng )
+    dist = 0.003
 
+    swLat = float(lat) - dist
+    swLng = float(lng) - dist
+    neLat = float(lat) + dist
+    neLng = float(lng) + dist
+
+    print float(swLat)
+    print float(swLng)
+    print float(neLat)
+    print float(neLng)
+
+    url = 'http://countdown.tfl.gov.uk/markers/swLat/%s/swLng/%s/neLat/%s/neLng/%s/?_dc=1315936072189' \
+        % ( swLat, swLng, neLat, neLng )
+
+    result = simplejson.load(urllib.urlopen(url))
+
+    print len(result['markers'])
+
+    stops = []
+
+    for item in result['markers']:
+        stops.append( { 'id':item['id'], 'name':item['name'], 'letter':item['stopIndicator'], 'lat':item['lat'], 'lng':item['lng'] } )
+
+    return json.dumps( stops )
                 
 
 @route('/')
@@ -45,7 +82,7 @@ def index():
         id = thestop[0]
 
         busstops.append( {'name':name,'id':id} )
-
+    
 
     return template('bus',{ 'stops' : busstops } )
 
--- public/js/script.js
+++ public/js/script.js
@@ -18,10 +18,12 @@ $(document).ready(function() {
         base.$el.data("bus", base);
         
         
-        base.getBus = function( routeID,callback )
+        base.markers = [];
+        
+        
+        base.getBus = function( stopID, routeID, callback )
         {
-            var stopID = 1045,
-                url = "/bus/" + stopID + '/' + routeID;
+            var url = "/bus/" + stopID + '/' + routeID;
                 console.log( url );
             
             $.getJSON(url, function( data ) {  
@@ -29,8 +31,168 @@ $(document).ready(function() {
             }); 
         }
         
+        base.getBusStops = function( lat,lng,callback )
+        {
+            var stopID = 1045,
+                url = "/stops/" + lat + '/' + lng;
+            
+            $.getJSON( encodeURI(url), function( data ) {  
+                callback( data );
+            }); 
+        }
+        
+        
+        /**
+         * Initialise retailers map
+         *
+         */
+        base.retailer_map = function( pos )
+        {
+            // Only run if google api is loaded
+            if( typeof(google) == "undefined" || typeof(google.maps.MapTypeId) == "undefined" )
+                return false;
+
+            // create a single info window object
+            infoWindow = new google.maps.InfoWindow();
+
+            //Google maps defaults
+            var myOptions = {
+                zoom: 15,
+                mapTypeId: google.maps.MapTypeId.ROADMAP
+            };
+
+            // Create map object in map_canvas element
+            base.map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+
+
+            var centre = new google.maps.LatLng( pos.coords.latitude, pos.coords.longitude );
+            base.map.setCenter( centre );
+
+            // add markers when the tiles are loaded
+            
+            base.getBusStops( pos.coords.latitude, pos.coords.longitude, function( data ){
+                
+                // store busstops in object so addMarkers can use them
+                base.busStops = data;
+                
+                // Create markers and attach them to the map
+                base.addMarkers();
+                
+                // Bind addMarkers method to tilesloaded event
+//                google.maps.event.addListenerOnce( base.map, 'tilesloaded', base.addMarkers );
+            });
+            
+            
+
+        }
+        
+        
+
+        /**
+         * Loop through array of locations "mymarkers" returned by the store_locator class in the footer of the page
+         *
+         */
+        base.addMarkers = function()
+        {
+            try{
+                if( mapdebug ){
+                    var paths = [
+                    new google.maps.LatLng(latbig, lngbig),
+                    new google.maps.LatLng(latbig, lngsm),
+                    new google.maps.LatLng(latsm, lngsm),
+                    new google.maps.LatLng(latsm, lngbig)
+                    ];
+
+                    var shape = new google.maps.Polygon({
+                    paths: paths,
+                    strokeColor: '#ff0000',
+                    strokeOpacity: 0.8,
+                    strokeWeight: 2,
+                    fillColor: '#ff0000',
+                    fillOpacity: 0.35
+                    });
+
+                    shape.setMap(map);
+                }
+            }catch(e){}
+            
+            // get busstops from webservice
+
+
+            var createMaker = function( map, myLatLng, id, title, i, letter )
+            {
+                
+                // Create marker object for the retailer
+                var marker = new google.maps.Marker({
+                    position: myLatLng,
+                    map: map,
+                    title: title,
+                    id: id,
+                    letter: letter
+                });
+                
+                // Store marker objects in an array to manipulate them later
+                base.markers[id] = marker;
+                
+                google.maps.event.addListener(marker, 'click', function( something,selse ) {
+                    
+                    base.getBus( id, 88, function( data ){
+                        
+                        var found = data.length;
+                        
+                        if( !found ){
+                            alert( 'Bugger looks like a long wait for a fucking ' + routeID + '!!' );
+                        }
+                            
+                        for( item in data ){
+                            var el = document.createElement( 'p' );
+                            el.id = item;
+                            el.innerHTML = data[item].route + ' ' + data[item].wait;
+                            base.$screen.append( $(el).hide().fadeIn() );
+                        }
+                        
+                    } );
+                });
+
+            }// /createMarker ----------------------------------------------------------
+
+            // Loop
+            var len = base.busStops.length;
+            
+            for (var i = 0; i < len; i++) {
+                stopLocation = base.busStops[i]; // curent item
+
+                // create a latlong object for the marker
+                var myLatLng = new google.maps.LatLng( stopLocation.lat, stopLocation.lng ),
+                    letter = stopLocation.letter,
+                    title = stopLocation.name + ' (' + letter + ')';
+
+                // Create and register maker
+                createMaker( base.map, myLatLng, stopLocation.id, title, i, letter );
+
+            }// /loop
+
+        }// /addMarkers ----------------------------------------------------------------
+
+
+        
+        
+        
         base.init = function(){
             base.options = $.extend({},$.bus.defaultOptions, options);
+            
+            // Get location and run retailermap as callback
+            navigator.geolocation.getCurrentPosition(
+                base.retailer_map,
+                errorGettingPosition,
+                {
+                    'enableHighAccuracy':true,
+                    'timeout':10000,
+                    'maximumAge':0
+                });
+            
+            
+            // Loop buses
             base.$buses.each( function(){
 
                 $(this).find('li a').bind( 'click', function(event){
@@ -73,3 +235,22 @@ $(document).ready(function() {
     };
     
 })(jQuery);
+
+
+
+
+// Callback functions for geo lookup
+// Error handler for geo lookup
+function errorGettingPosition(err)
+{
+    if(err.code==1){
+	alert("User denied geolocation.");
+    }else if(err.code==2){
+	alert("Position unavailable.");
+    }else if(err.code==3){
+	alert("Timeout expired.");
+    }else{
+	alert("ERROR:"+ err.message);
+    }
+}
+
