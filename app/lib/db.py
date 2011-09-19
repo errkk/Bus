e3b2a722cd842f7d87947f2dc44a49d46b14cf87
Eric <errkkgeorge+github@gmail.com>
2011-09-19 23:25:31 +0100

    Added DB Class

--- app/lib/db.py
+++ app/lib/db.py
@@ -18,3 +18,26 @@ class Database(object):
             return True
         except Database:
             return false
+
+    def findStops(self,swLat,swLng,neLat,neLng):
+
+
+#        print swLat,swLng,neLat,neLng
+#        51.4591 -0.1396 51.4651 -0.1312
+#        condition = {'lat':{'$gt': float(swLat), '$lt': float(neLat)},'lng':{'$gt': float(swLng), '$lt': float(neLng)}}
+        condition = {'direction':99}
+        
+#        , '$lt':neLat},'lng':{'$gt': swLng, '$lt':neLng
+
+
+#        cur = self.col.busstops.find( {'lng':{'$gt': swLat}} )
+
+        try:
+            for i in self.col.busstops.find( {'direction':99} ):
+                print i
+        except Database:
+            print 'Error'
+
+
+        
+#        print self.col.busstops.find( condition ).sort("id").count()
